2cbc51128398018eab078756ef266129d8965884
Midhun M S <midhunreal1@gmail.com>
2023-08-30 22:02:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  const [time,start,stop,reset] =useStopWatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="stopwatch">
+        <div className="time">{time}</div>
+        <div className="buttons">
+          <button className="action-button" onClick={start}>
+            Start
+          </button>
+          <button className="action-button" onClick={stop}>
+            Stop
+          </button>
+          <button className="action-button" onClick={reset}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+ 
+function useStopWatch(){
+  const interval =useRef(0)
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const start = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      interval.current = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+    }}
+  const stop = () => {
+      clearInterval(interval.current);
+      setIsRunning(false);
+    }
+  const reset = ()=>{
+      stop();
+      setTime(0)
+  }
+  return [time,start,stop,reset]
+  };
